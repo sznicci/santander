0953e6561d817a54e5c979fd96bb297424e98a3a
unknown <sznicci@gmail.com>
2018-06-06 16:21:42 +0100

    Create new java file for creating the Status table. Create test for testing Status table.

---
+++ FinalProject_maven/src/main/java/org/santander/finalproject_maven/StatusTable.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.santander.finalproject_maven;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Research
+ */
+public class StatusTable {
+    
+    private static final String SQL_CREATE_STATUS_TABLE = "CREATE TABLE status (\n"
+            + "id serial PRIMARY KEY,\n"
+            + "station_id int,\n"
+            + "date date,\n"
+            + "time time,\n"
+            + "capacity int,\n"
+            + "takeout int,\n"
+            + "return int,\n"
+            + "available int,\n"
+            + "status int\n"
+            + ");";
+    
+    protected static void createStatusTable(Connection con) {
+        try (PreparedStatement ps = con.prepareStatement(SQL_CREATE_STATUS_TABLE)) {
+            ps.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static String getSQL_CREATE_STATUS_TABLE() {
+        return SQL_CREATE_STATUS_TABLE;
+    }
+    
+}
